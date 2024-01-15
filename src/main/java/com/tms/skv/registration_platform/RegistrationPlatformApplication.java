8ab2873ead0e6d42575d4f3b033b6d45e97be455
Sviatlana.Kryskevich <sviatlana.kryskevich@gmail.com>
2024-01-15 13:04:59 +0100

    structure-build

---
+++ src/main/java/com/tms/skv/registration_platform/RegistrationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.tms.skv.registration_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationPlatformApplication.class, args);
+    }
+
+}
